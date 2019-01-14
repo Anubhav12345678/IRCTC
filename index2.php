7630c1205b80f025093e1b33e7920e1cf3123fef
Anubhav12345678 <45006874+Anubhav12345678@users.noreply.github.com>
2019-01-14 16:18:26 +0530

    my index page

---
+++ index2.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>Allahabad Railways</title>
+	<style type="text/css">
+		#s{
+			position: absolute;
+			top: 80px;
+			left: 600px;
+			height: 450px;
+			width: 400px;
+			box-shadow: 2px 2px;
+			border: 1px solid #000000;
+			background-color: #FFFFFF;
+		}
+		span
+		{
+			font-family: "Montserrat",sans-serif;
+		}
+
+		.b
+		{
+			font-size: 20px;
+		}
+		#d
+		{
+			position: relative;
+			left: 40px;
+			font-family: "Montserrat",sans-serif;
+		}
+		#e
+		{
+			position: absolute;
+			left: 550px;
+		}
+
+
+
+
+
+	</style>
+</head>
+<body bgcolor="#C9C9C9">
+    <span style="position: absolute; left: 620px; font-size: 40px;">Allahabad Railways</span>
+    <div id="s">
+    	<div align="center" id="a">
+    		WELCOME TO<br> ALLAHABAD RAILWAYS<br>
+    		<i class="fa fa-train"></i><br><br><br>
+    	</div>
+
+    	<form action="login.php" method="POST">
+    		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>USERNAME</span><br>
+    		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="username" class="b" size="34" required><br><br>
+    		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>PASSWORD</span><br>
+    		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="password" name="password" class="b" size="34" required><br><br>
+    		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="submit" name="c" value="LOGIN" id="d"><br><br>
+
+
+
+
+    	</form>
+
+        <a href="http://localhost/mnp/signup.php"><button style="font-family: 'Montserrat',sans-serif;">REGISTER</button><br></a>
+    		<a href="https://twitter.com/IRCTCofficial"><img src="twitter.jfif" style="position: absolute; height: 30px; width: 30px; left: 160px; top: 400px;"></a>
+    		<a href="https://www.facebook.com/IRCTCofficial/"><img src="tw.jfif" style="position: absolute; height: 30px; width: 30px; left: 240px; top: 400px;"></a>
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+
+</body>
+</html>
